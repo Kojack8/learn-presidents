e100147afdac326fb478d6723f556f500a4cd61d
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-02-11 16:39:27 -0800

    Added question class

---
+++ db/migrate/20210212002908_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :questions do |t|
+      t.string :name
+      t.text :hint_1
+      t.text :hint_2
+      t.text :hint_3
+
+      t.timestamps
+    end
+  end
+end
