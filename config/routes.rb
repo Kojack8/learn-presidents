edb94799c0719e83c1702b7dd5b0a8f7dabd8c40
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-02-11 16:49:56 -0800

    Added questions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "welcome#index"
-  
+
+  get "/questions", to:"questions#index"
   get "/welcome", to: "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
