4587f9451da75e91cfe0549da6bf3823d20814d2
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-02-12 17:21:47 -0800

    Added new and create funct

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
-  get "/questions", to:"questions#index"
   get "/welcome", to: "welcome#index"
+  get "/questions", to: "questions#index"
+  get "/questions/new", to: "questions#new"
+  get "/questions/:id", to: "questions#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
