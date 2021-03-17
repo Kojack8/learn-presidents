c9680fd90d5fecaa068e8ce2d8f686d1e9e7ea72
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-17 15:13:22 -0500

    Hint button works correctly

--- app/javascript/packs/question.js
+++ app/javascript/packs/question.js
@@ -1,9 +1,26 @@
 document.getElementById("myGuess").addEventListener("submit", checkAnswer)
-document.getElementById("hint").addEventListener("click",revealHint)
+document.getElementById("hint").addEventListener("click", revealHint)
 
 function revealHint() {
-  var x = 1
-  alert(x)
+  var hint_1 = document.getElementById("hint-1");
+  var hint_2 = document.getElementById("hint-2");
+  var hint_3 = document.getElementById("hint-3");
+
+  
+  if (window.getComputedStyle(hint_1).display === 'none'){
+    hint_1.style.display = 'block';
+  }
+  else if (window.getComputedStyle(hint_2).display === 'none'){
+    hint_2.style.display = 'block';
+  }
+  else if (window.getComputedStyle(hint_3).display === 'none'){
+    hint_3.style.display = 'block';
+  }
+  else{
+    alert("nah")
+  }
+  
+
 }
 
 
