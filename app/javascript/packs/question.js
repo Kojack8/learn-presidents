a5db04a7059deb1921fc9ded434dd23def430d92
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-17 12:12:51 -0500

    Added hint button

--- app/javascript/packs/question.js
+++ app/javascript/packs/question.js
@@ -1,10 +1,18 @@
 document.getElementById("myGuess").addEventListener("submit", checkAnswer)
+document.getElementById("hint").addEventListener("click",revealHint)
+
+function revealHint() {
+  var x = 1
+  alert(x)
+}
+
 
 function checkAnswer() {
   
   var guess = document.getElementById("guess").value;
   if (guess != 'George Washington'){
-    alert(js_answer)
+    alert(guess)
   }
 }
+
 
