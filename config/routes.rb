d62d2fbcfe83ba2832227abecdb97076620e9153
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-23 20:41:01 -0500

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
 
   get "/welcome", to: "welcome#index"
-  get "/questions", to: "questions#index"
-  get "/questions/answer", to: "questions#answer"
-  get "/questions/:id", to: "questions#show"
+  resources :questions, only: [:index, :show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
